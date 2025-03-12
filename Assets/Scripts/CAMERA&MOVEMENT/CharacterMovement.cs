9f731d7682c01e8fc1214b52bd73b468701abec4
Angelus0z <adamart20@gmail.com>
2025-03-12 11:37:12 +0000

    Init commit

---
+++ Assets/Scripts/CAMERA&MOVEMENT/CharacterMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController Controller;
+
+    public float speed = 5f;
+    public float turnSpeed = 180f;
+
+
+
+
+
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir;
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        moveDir = transform.forward * Input.GetAxis("Vertical") * speed;
+
+        Controller.Move(moveDir * Time.deltaTime - Vector3.up * 0.1f);
+    }
+}
