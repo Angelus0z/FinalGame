615cf77ffbb8850fafc7330fc4badb14998142b0
JudeEWalsh <walshjude63@gmail.com>
2025-03-17 19:20:09 +0000

    Judd Prefab Added
    Added Judd's character model and configured the controller to move him around. Note that animations are set up and applied but are currently not working correctly. Located in the Prefab folder.

--- Assets/Scripts/CAMERA&MOVEMENT/CharacterMovement.cs
+++ Assets/Scripts/CAMERA&MOVEMENT/CharacterMovement.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     private CharacterController Controller;
+    [SerializeField]private Animator Animator;
 
     public float speed = 5f;
     public float turnSpeed = 180f;
 
-
-
-
-
     void Start()
     {
         Controller = GetComponent<CharacterController>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,5 +24,10 @@ public class CharacterMovement : MonoBehaviour
         moveDir = transform.forward * Input.GetAxis("Vertical") * speed;
 
         Controller.Move(moveDir * Time.deltaTime - Vector3.up * 0.1f);
+
+        if(speed >= 0.01f)
+        {
+            //GetComponent<Animator>().SetBool("IsMoving") = true;
+        }
     }
 }
