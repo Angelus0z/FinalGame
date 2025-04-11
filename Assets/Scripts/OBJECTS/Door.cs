5ff215764cdbfb22b0b9d938dc2908af3882a121
Angelus0z <adamart20@gmail.com>
2025-04-11 19:46:36 +0100

    New End Scene
    This code has the door lock code and the new end scene

--- Assets/Scripts/OBJECTS/Door.cs
+++ Assets/Scripts/OBJECTS/Door.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    
+    /*public GameObject DoubleDoorPrefab;
+    [SerializeField] private ItemCollector itemCollector;
 
+    private void Start()
+    {
+        itemCollector = DoubleDoorPrefab.GetComponent<ItemCollector>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
 
+        if (other.tag == "Player")
+        {
+            if (collectedKeys >= 1)
+            {
+
+                GetComponent<Animator>().Play("DoubleDoorOpen");
+                collectedKeys--;
+                //  GameObject.Find("KeyAmount").GetComponent<Text>().text = " : " + collectedKeys;
+            }
+
+            else
+            {
+
+                print("You need more keys to unlock the gate!");
+                other.GetComponent<BoxCollider>().isTrigger = false;
+            }
+        }
+    }*/
 }
